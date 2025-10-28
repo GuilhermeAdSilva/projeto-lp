7d712fcd7af8c4a4407814c71a28a55e89245c67
PedroBiittencourt <phscooby9@gmail.com>
2025-10-28 01:05:01 -0300

    qualquer coisa

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -4,6 +4,8 @@ function Teste() {
         <div>
             <h1>Projeto LP2</h1>
             <p>O Guilherme é lindo!</p>
+            <p>Mas o Pedro é lindo e gostoso!</p>
+
         </div>
     )
 }
