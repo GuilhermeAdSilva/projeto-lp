dc377c294713c7a76f857db2f59f6bd34122a96c
Guilherme <guilhermeadrianodasilva69@gmail.com>
2025-10-30 03:07:01 -0300

    tentativa listagem

---
+++ src/config/axios.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://my-json-server.typicode.com/marcoaparaujo/jsonfake';
+export const BASE_URL2 = 'https://my-json-server.typicode.com/marcoaparaujo/jsonfake2';
+export const BASE_URL3 = 'https://my-json-server.typicode.com/marcoaparaujo/jsonfake3';
+export const BASE_URL4 = 'https://my-json-server.typicode.com/marcoaparaujo/jsonfake4';
